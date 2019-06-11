cea87b89853d41e296fa49548d7ca15d17bfb1d4
Rob Windsor <rob.windsor@gmail.com>
2019-06-11 10:19:32 -0400

    Update project files for Ex 1

--- Demos/01-webpart/src/webparts/reactWebPartDemo/components/ColorList.tsx
+++ Demos/01-webpart/src/webparts/reactWebPartDemo/components/ColorList.tsx
@@ -1,20 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 import * as React from 'react';
 import { IColor } from '../IColor';
 
 export interface IColorListProps {
-  colors: IColor[];
+    colors: IColor[];
 }
 
 export class ColorList extends React.Component<IColorListProps, {}> {
-  public render(): React.ReactElement<IColorListProps> {
-    return (
-      <ul>
-        {
-          this.props.colors.map(colorItem => (
-            <li>{ colorItem.title }</li>
-          ))
-        }
-      </ul>
-    );
-  }
+    public render(): React.ReactElement<IColorListProps> {
+        return (
+            <ul>
+                {
+                    this.props.colors.map(colorItem => (
+                        <li>{colorItem.title}</li>
+                    ))
+                }
+            </ul>
+        );
+    }
 }
